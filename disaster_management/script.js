dc122f135b5f115d93d1ec671af5fb6b67ff1aab
David Arhin <davidarhin2005@gmail.com>
2023-12-24 23:00:40 +0000

    style

--- disaster_management/script.js
+++ disaster_management/script.js
@@ -2,5 +2,5 @@ function helpCall(){
     blurryContainer = document.getElementById('blurry-container');
     blurryContainer.style.width = '100%';
     helpType=document.getElementById("help-type").value;
-    document.getElementById("info").innerHTML = "Are you sure it is <br> <h3>"+helpType+"</h3>";
+    document.getElementById("info").innerHTML = "Are you sure it is <br> <h3><i>"+helpType+"</i></h3>";
 }
