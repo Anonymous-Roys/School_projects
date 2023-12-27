2a648a8ce2a07ae771f63d3087606d8a74597799
David Arhin <davidarhin2005@gmail.com>
2023-12-27 22:38:11 +0000

    close button

--- disaster_management/script.js
+++ disaster_management/script.js
@@ -1,6 +1,11 @@
 function helpCall(){
     blurryContainer = document.getElementById('blurry-container');
-    blurryContainer.style.width = '100%';
+    blurryContainer.style.display = 'flex';   
     helpType=document.getElementById("help-type").value;
     document.getElementById("info").innerHTML = "Are you sure it is <br> <h3><i>"+helpType+"</i></h3>";
+}
+
+const closeBtn = () => {
+    blurryContainer = document.getElementById('blurry-container');
+    blurryContainer.style.display = 'none';   
 }
